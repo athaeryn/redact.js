2f08ed5f2d5142e8596dc0e26b33f94fdbf77b9a
Mike Anderson <athaeryn@me.com>
2013-03-14 14:55:46 -0500

    Refactoring and commenting.

--- redact.js
+++ redact.js
@@ -1,26 +1,40 @@
-$(document).ready( function () {
+$(document).ready(function () {
+    "use strict";
+
+    var color,
+        chars = ['*', '&', '#', '@', '!', '%'];
+
     function randChar() {
-        var chars = ['*', '&', '#', '@', '!', '%'];
         return chars[Math.floor(Math.random() * chars.length)];
     }
-    var redactedblocks = $('.redacted'), color;
-    redactedblocks.each(function (i) {
-        color = $(this).css('color');
-        var original = $(this).html();
-        var grawlix = '';
+
+    $('.redacted').each(function () {
+        // Get the original text
+        var original = $(this).html(),
+            grawlix = '';
+
         for (var i = 0; i < original.length; i++) {
             grawlix += original[i] == ' ' ? ' ' : randChar();
         }
+
         var split = grawlix.split(' ');
         for (var i = 0; i < split.length; i++) {
             split[i] = '<span>' + split[i] + '</span>';
         }
+
         $(this).html(split.join(' '));
+
+        // Save the color
+        color = $(this).css('color');
+
+        // Change it to transparent. Also, position relative for the :afters.
+        $(this).find('span').css({
+            'color': 'transparent',
+            'position': 'relative'
+        });
     });
-    redactedblocks.find('span').css({
-        'color': 'transparent',
-        'position': 'relative'
-    });
+
     var style = "<style>.redacted>span:after{position:absolute;top:25%;bottom:25%;left:0;right:0;background:" + color + ";content:'';}</style>"
     $('head').append(style);
+
 });
