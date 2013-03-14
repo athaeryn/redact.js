f39cc87697b6f80d2f6e7301f03330f86e191a1e
Mike Anderson <athaeryn@me.com>
2013-03-14 10:54:17 -0500

    Grawlix is more fun that just asterisks.

--- redact.js
+++ redact.js
@@ -1,10 +1,17 @@
 $(document).ready( function () {
+    function randChar() {
+        var chars = ['*', '&', '#', '@', '!', '%'];
+        return chars[Math.floor(Math.random() * chars.length)];
+    }
     var redactedblocks = $('.redacted'), color;
     redactedblocks.each(function (i) {
         color = $(this).css('color');
-        var content = $(this).html();
-        content = content.replace(/\w/g, '*');
-        var split = content.split(' ');
+        var original = $(this).html();
+        var grawlix = '';
+        for (var i = 0; i < original.length; i++) {
+            grawlix += original[i] == ' ' ? ' ' : randChar();
+        }
+        var split = grawlix.split(' ');
         for (var i = 0; i < split.length; i++) {
             split[i] = '<span>' + split[i] + '</span>';
         }
