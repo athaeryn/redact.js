19ef91dcb178dd8f936b83ba58dcc7757382d629
Mike Anderson <athaeryn@me.com>
2013-03-15 11:37:16 -0500

    Refactored. Non-breaking space is now respected.

--- redact.js
+++ redact.js
@@ -9,31 +9,46 @@ $(document).ready(function () {
     }
 
     $('.redacted').each(function () {
-        // Get the original text
-        var original = $(this).html(),
-            grawlix = '';
+        // Get the original text of the outer element
+        var all = $(this).html();
 
-        for (var i = 0; i < original.length; i++) {
-            grawlix += original[i] == ' ' ? ' ' : randChar();
-        }
+        // Wrap each word in <span>
+        all = all.replace(/\s+/g, '</span> <span>');
+        all = all.replace(/&nbsp;/g, '</span>&nbsp;<span>');
+        all = '<span>' + all + '</span>';
 
-        var split = grawlix.split(' ');
-        for (var i = 0; i < split.length; i++) {
-            split[i] = '<span>' + split[i] + '</span>';
-        }
+        // Put the <span>-wrapped content back
+        $(this).html(all);
 
-        $(this).html(split.join(' '));
+        // Go through each of these new spans
+        $(this).find('span').each(function () {
+            
+            // Grab the old content
+            var old = $(this).html(),
+                grawlix = '';
 
+            // Replace each letter with a random character
+            for (var i = 0; i < old.length; i++) {
+                grawlix += old[i] == ' ' ? ' ' : randChar();
+            }
+
+            // Set the content to the grawlixes
+            $(this).html(grawlix);
+
+            // Set some stylings
+            $(this).css({
+                'color': 'transparent',
+                'position': 'relative'
+            });
+        });
+        
         // Save the color
         color = $(this).css('color');
 
         // Change it to transparent. Also, position relative for the :afters.
-        $(this).find('span').css({
-            'color': 'transparent',
-            'position': 'relative'
-        });
     });
 
+    // Inject the blocks' style, so they are sexy and thin
     var style = "<style>.redacted>span:after{position:absolute;top:25%;bottom:25%;left:0;right:0;background:" + color + ";content:'';}</style>"
     $('head').append(style);
 
