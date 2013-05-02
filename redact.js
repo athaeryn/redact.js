a5aba3e5cc877c569ad868dd48cf71f16f5d42c6
Mike Anderson <athaeryn@me.com>
2013-05-02 17:14:38 -0500

    Jslinted.

--- redact.js
+++ redact.js
@@ -1,37 +1,42 @@
+/*jslint browser: true */
+/*globals $ */
 $(document).ready(function () {
     "use strict";
- 
-    // This give arrays "pushUnique" function
-    // If the item you wish to push exists in the array, then you get back its
+
+    // This give arrays "pushUnique" function // If the item you wish to push exists in the array, then you get back its
     // id, which is just its position in the array. If it doesn't exist, then it
     // gets pushed on to the array, and you still get back the id.
     Array.prototype.pushUnique = function (item) {
         var id = this.indexOf(item);
-        if (id == -1) {
+        if (id === -1) {
             this.push(item);
             id = this.length - 1;
         }
         return id;
-    }
+    };
 
     // "color" will hold the ids of each unique color.
     var color = [],
-        chars = '*&#@!%'.split('');
+        chars = ['*', '&', '#', '@', '!', '%'],
+        // Set the common style for the blocks.
+        style = ".redacted>span:after{position:absolute;top:25%;bottom:25%;left:0;right:0;content:'';}",
+        i;
 
     // Simply returns a random character from the "chars" array.
     function randChar() {
         return chars[Math.floor(Math.random() * chars.length)];
     }
 
-    // "This"->"!@&$" 
+    // "This"->"!@&$"
     function grawlix(text) {
-        var grawlix = '';
-        for (var i = 0; i < text.length; i++) {
-            grawlix += text[i] == ' ' ? ' ' : randChar();
+        var grawlixed = '',
+            i;
+        for (i = 0; i < text.length; i += 1) {
+            grawlixed += text[i] === ' ' ? ' ' : randChar();
         }
-        return grawlix;
+        return grawlixed;
     }
-   
+
     // Wraps each word in a <span>, and is repectful of &nbsp;
     function spanify(text) {
         return text.replace(/\S+/g, function (a) {
@@ -49,7 +54,7 @@ $(document).ready(function () {
 
         // Go through each of these new <span>s.
         $(this).find('span').each(function () {
-            
+
             // Set the content to grawlixes.
             $(this).html(grawlix($(this).html()));
 
@@ -61,16 +66,13 @@ $(document).ready(function () {
             });
         });
 
-        // Attach a hook class with the appropriate unique color id 
+        // Attach a hook class with the appropriate unique color id
         $(this).addClass('redacted' + colorID);
     });
 
-    // Set the common style for the blocks.
-    var style = ".redacted>span:after{position:absolute;top:25%;bottom:25%;left:0;right:0;content:'';}"
-
     // Create rules for each unique color.
-    for (var i = 0; i < color.length; i++) {
-        style += ".redacted" + i + ">span:after{background:" + color[i] + "}"
+    for (i = 0; i < color.length; i += 1) {
+        style += ".redacted" + i + ">span:after{background:" + color[i] + "}";
     }
 
     // Insert the style tag into the document.
